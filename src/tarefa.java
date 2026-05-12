4da10cf95c0604d5c9585e112e9783c6370b0330
Lucas <lucasmachadotellesdaluz@gmail.com>
2026-05-12 20:09:24 -0300

    adicionando as mesmas cosias

--- src/tarefa.java
+++ src/tarefa.java
@@ -1,10 +1,12 @@
 public class tarefa {
     String descricao;
     boolean concluida;
+    String prioridade;  // NOVO
 
     public tarefa(String descricao){ // construtor
         this.descricao = descricao;
         this.concluida = false;
+        this.prioridade = "Baixa"; // VALOR PADRAO
     }
 
 
