fba4e9c056fcb2c84a1a761aa99006515c9aef42
Lucas <lucasmachadotellesdaluz@gmail.com>
2026-05-12 20:04:29 -0300

    Adicionando sistema de prioridade

---
+++ src/tarefa.java
@@ -0,0 +1,11 @@
+public class tarefa {
+    String descricao;
+    boolean concluida;
+
+    public tarefa(String descricao){ // construtor
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+
+}
